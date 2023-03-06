f223d9d699bc6d4cdb9d4ab2dd0a4410d5871e58
eduardo4563 <eduardomontanocondemayta@gmail.com>
2023-03-05 23:30:23 -0500

    cambios

---
+++ js/animar.js
@@ -0,0 +1,12 @@
+let animado=document.querySelectorAll(".animado");
+function mostrarscrool(){
+    let scrooltop=document.documentElement.scrollTop;
+    for(var i=0;i<animado.length;i++){
+        let alturaanimasdo=animado[i].offsetTop;
+        if(alturaanimasdo - 600 < scrooltop){
+            animado[i].style.opacity=1;
+            animado[i].classList.add("mostraanimado")
+        }
+    }
+}
+window.addEventListener('scroll',mostrarscrool)
